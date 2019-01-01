1a2eef95184c5815027bd62bf7c9dec0e2e674d4
Bradley Grainger <bgrainger@gmail.com>
2019-01-01 10:04:33 -0800

    Detect ToReadOnlyCollection in constructors. Fixes #17

---
+++ src/Faithlife.Analyzers/ToReadOnlyCollectionAnalyzer.cs
@@ -0,0 +1,62 @@
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Faithlife.Analyzers
+{
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public sealed class ToReadOnlyCollectionAnalyzer : DiagnosticAnalyzer
+	{
+		public override void Initialize(AnalysisContext analysisContext)
+		{
+			analysisContext.RegisterCompilationStartAction(compilationStartAnalysisContext =>
+			{
+				var enumerableUtility = compilationStartAnalysisContext.Compilation.GetTypeByMetadataName("Libronix.Utility.EnumerableUtility");
+				if (enumerableUtility != null)
+				{
+					compilationStartAnalysisContext.RegisterOperationBlockStartAction(context =>
+					{
+						if (context.OwningSymbol is IMethodSymbol methodSymbol && methodSymbol.MethodKind == MethodKind.Constructor)
+							context.RegisterOperationAction(oc => AnalyzeOperation(oc, enumerableUtility), OperationKind.Invocation);
+					});
+				}
+			});
+		}
+
+		public const string DiagnosticId = "FL0005";
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s_rule);
+
+		private static void AnalyzeOperation(OperationAnalysisContext context, INamedTypeSymbol enumerableUtility)
+		{
+			var invocationOperation = (IInvocationOperation) context.Operation;
+			var targetMethod = invocationOperation.TargetMethod;
+			if (targetMethod.ContainingType == enumerableUtility && targetMethod.Name == "ToReadOnlyCollection")
+			{
+				var expressionStatement = invocationOperation.Syntax.FirstAncestorOrSelf<ExpressionStatementSyntax>();
+				if (expressionStatement?.Expression is AssignmentExpressionSyntax assignmentExpression)
+				{
+					if (assignmentExpression.Left is IdentifierNameSyntax identifierName)
+					{
+						var semanticModel = context.Compilation.GetSemanticModel(identifierName.SyntaxTree);
+						var symbolInfo = semanticModel.GetSymbolInfo(identifierName);
+						var kind = symbolInfo.Symbol.Kind;
+						if (kind == SymbolKind.Field || kind == SymbolKind.Property)
+							context.ReportDiagnostic(Diagnostic.Create(s_rule, invocationOperation.GetMethodNameLocation()));
+					}
+				}
+			}
+		}
+
+		static readonly DiagnosticDescriptor s_rule = new DiagnosticDescriptor(
+			id: DiagnosticId,
+			title: "ToReadOnlyCollection in constructor",
+			messageFormat: "Avoid ToReadOnlyCollection in constructors.",
+			category: "Usage",
+			defaultSeverity: DiagnosticSeverity.Warning,
+			isEnabledByDefault: true,
+			helpLinkUri: $"https://github.com/Faithlife/FaithlifeAnalyzers/wiki/{DiagnosticId}");
+	}
+}
---
+++ src/Faithlife.Analyzers/ToReadOnlyCollectionCodeFixProvider.cs
@@ -0,0 +1,64 @@
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Simplification;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace Faithlife.Analyzers
+{
+	[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(CurrentAsyncWorkItemCodeFixProvider)), Shared]
+	public sealed class ToReadOnlyCollectionCodeFixProvider : CodeFixProvider
+	{
+		public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(ToReadOnlyCollectionAnalyzer.DiagnosticId);
+
+		public override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+		public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+		{
+			var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+			var semanticModel = await context.Document.GetSemanticModelAsync(context.CancellationToken).ConfigureAwait(false);
+
+			var diagnostic = context.Diagnostics.First();
+			var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+			var invocation = root.FindNode(diagnosticSpan).FirstAncestorOrSelf<InvocationExpressionSyntax>();
+			if (invocation is null)
+				return;
+
+			var methodSymbol = semanticModel.GetSymbolInfo(invocation).Symbol as IMethodSymbol;
+			if (methodSymbol is null)
+				return;
+
+			context.RegisterCodeFix(
+				CodeAction.Create(
+					title: "Use .ToList().AsReadOnly()",
+					createChangedDocument: token => ConvertToToListAsReadOnlyAsync(context.Document, invocation, token),
+					"to-list-as-read-only"),
+				diagnostic);
+		}
+
+		private static async Task<Document> ConvertToToListAsReadOnlyAsync(Document document, InvocationExpressionSyntax invocation, CancellationToken cancellationToken)
+		{
+			var root = (await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false))
+				.ReplaceNode(invocation, InvocationExpression(
+						MemberAccessExpression(
+							SyntaxKind.SimpleMemberAccessExpression,
+							InvocationExpression(
+								MemberAccessExpression(
+									SyntaxKind.SimpleMemberAccessExpression,
+									((MemberAccessExpressionSyntax) invocation.Expression).Expression,
+									IdentifierName("ToList"))),
+							IdentifierName("AsReadOnly")))
+					.NormalizeWhitespace());
+
+			return await Simplifier.ReduceAsync(document.WithSyntaxRoot(root), cancellationToken: cancellationToken).ConfigureAwait(false);
+		}
+	}
+}
