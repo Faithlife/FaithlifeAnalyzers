3e6435fc3f1e48e581fce9a134078c136b2155e5
Ed Ball <ed.ball@faithlife.com>
2019-01-03 16:21:01 -0800

    Implement analyzer for OrderBy on string.

---
+++ src/Faithlife.Analyzers/OverloadWithStringComparerAnalyzer.cs
@@ -0,0 +1,80 @@
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Faithlife.Analyzers
+{
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public sealed class OverloadWithStringComparerAnalyzer : DiagnosticAnalyzer
+	{
+		public override void Initialize(AnalysisContext context)
+		{
+			context.RegisterCompilationStartAction(compilationStartAnalysisContext =>
+			{
+				var enumerableType = compilationStartAnalysisContext.Compilation.GetTypeByMetadataName("System.Linq.Enumerable");
+				if (enumerableType != null)
+				{
+					compilationStartAnalysisContext.RegisterOperationAction(operationContext =>
+					{
+						var operation = (IInvocationOperation) operationContext.Operation;
+						var targetMethod = operation.TargetMethod;
+
+						if (targetMethod != null &&
+							targetMethod.ContainingType == enumerableType &&
+							(targetMethod.Name == "OrderBy" || targetMethod.Name == "OrderByDescending" || targetMethod.Name == "ThenBy" || targetMethod.Name == "ThenByDescending") &&
+							targetMethod.TypeArguments.Length == 2 &&
+							targetMethod.TypeArguments[1].SpecialType == SpecialType.System_String &&
+							targetMethod.Parameters.Length == 2)
+						{
+							operationContext.ReportDiagnostic(Diagnostic.Create(s_useStringComparerRule, GetMethodNameLocation(operation.Syntax)));
+						}
+					}, OperationKind.Invocation);
+				}
+			});
+		}
+
+		public const string UseStringComparerDiagnosticId = "FL0006";
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => s_rules;
+
+		private static Location GetMethodNameLocation(SyntaxNode syntaxNode)
+		{
+			switch (syntaxNode)
+			{
+			case InvocationExpressionSyntax invocation:
+				var expression = invocation.Expression;
+				switch (expression)
+				{
+				case MemberAccessExpressionSyntax memberAccessExpression:
+					return memberAccessExpression.Name.GetLocation();
+				case ConditionalAccessExpressionSyntax conditionalAccessExpression:
+					return conditionalAccessExpression.WhenNotNull.GetLocation();
+				case InvocationExpressionSyntax invocationExpression:
+					return invocationExpression.GetLocation();
+				default:
+					return syntaxNode.GetLocation();
+				}
+
+			case OrderingSyntax ordering:
+				return ((OrderByClauseSyntax) ordering.Parent).OrderByKeyword.GetLocation();
+
+			default:
+				return syntaxNode.GetLocation();
+			}
+		}
+
+		static readonly DiagnosticDescriptor s_useStringComparerRule = new DiagnosticDescriptor(
+			id: UseStringComparerDiagnosticId,
+			title: "Use IComparer<string> overload",
+			messageFormat: "Use the overload that takes an IComparer<string>.",
+			category: "Usage",
+			defaultSeverity: DiagnosticSeverity.Warning,
+			isEnabledByDefault: true,
+			description: "The desired comparer must be explicitly specified. Consider StringComparer.Ordinal.",
+			helpLinkUri: $"https://github.com/Faithlife/FaithlifeAnalyzers/wiki/{UseStringComparerDiagnosticId}");
+
+		static readonly ImmutableArray<DiagnosticDescriptor> s_rules = ImmutableArray.Create(s_useStringComparerRule);
+	}
+}
