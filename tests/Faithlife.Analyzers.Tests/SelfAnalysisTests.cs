3c4a36f100a26a47128b22932e56a812f5721797
Bradley Grainger <bgrainger@gmail.com>
2019-02-16 14:47:18 -0800

    Check Faithlife.Analyzers with Faithlife.Analyzers.

---
+++ tests/Faithlife.Analyzers.Tests/SelfAnalysisTests.cs
@@ -0,0 +1,69 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Text;
+using NUnit.Framework;
+
+namespace Faithlife.Analyzers.Tests
+{
+	[TestFixture]
+	public class SelfAnalysisTests
+	{
+		[Test]
+		public async Task CheckFaithlifeAnalyzers()
+		{
+			const string projectName = "Faithlife.Analyzers";
+			var projectId = ProjectId.CreateNewId(debugName: projectName);
+
+			var workspace = new AdhocWorkspace();
+			var solution = workspace
+				.CurrentSolution
+				.AddProject(projectId, projectName, projectName, LanguageNames.CSharp)
+				.WithProjectCompilationOptions(projectId, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))
+				.AddMetadataReferences(projectId, s_metadataReferences);
+
+			foreach (var source in Directory.GetFiles(@"../../../../../src/Faithlife.Analyzers", "*.cs"))
+			{
+				var fileName = Path.GetFileName(source);
+				var documentId = DocumentId.CreateNewId(projectId, debugName: fileName);
+				solution = solution.AddDocument(documentId, fileName, SourceText.From(File.OpenRead(source)), filePath: source);
+			}
+
+			var project = solution.GetProject(projectId);
+			var compilation = await project.GetCompilationAsync();
+			var compilationWithAnalyzers = compilation.WithAnalyzers(ImmutableArray.Create<DiagnosticAnalyzer>(
+				new CurrentAsyncWorkItemAnalyzer(),
+				new OverloadWithStringComparerAnalyzer(),
+				new OverloadWithStringComparisonAnalyzer(),
+				new StringAnalyzer(),
+				new ToReadOnlyCollectionAnalyzer(),
+				new UntilCanceledAnalyzer()
+			));
+
+			var diagnostics = await compilationWithAnalyzers.GetAnalyzerDiagnosticsAsync();
+			Assert.IsEmpty(diagnostics);
+		}
+
+		private static readonly IReadOnlyList<string> s_assemblyReferences = new[]
+		{
+			"System.Collections",
+			"System.Collections.Immutable",
+			"System.Composition.AttributedModel",
+			"System.Linq",
+			"System.Private.CoreLib",
+			"System.Runtime",
+			"Microsoft.CodeAnalysis",
+			"Microsoft.CodeAnalysis.CSharp",
+			"Microsoft.CodeAnalysis.Workspaces",
+		};
+
+		private static readonly IReadOnlyList<MetadataReference> s_metadataReferences = s_assemblyReferences
+			.Select(x => (MetadataReference) MetadataReference.CreateFromFile(Assembly.Load(x).Location)).ToList();
+	}
+}
