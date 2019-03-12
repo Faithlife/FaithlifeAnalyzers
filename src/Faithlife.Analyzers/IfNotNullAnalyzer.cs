5f961aef91b7c09d492e4a3a6fcf04417120fe4c
David Mitchell <dmitchell@logos.com>
2019-03-12 10:03:39 -0700

    Add IfNotNull analyzer and code fixer.

---
+++ src/Faithlife.Analyzers/IfNotNullAnalyzer.cs
@@ -0,0 +1,55 @@
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Faithlife.Analyzers
+{
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public sealed class IfNotNullAnalyzer : DiagnosticAnalyzer
+	{
+		public override void Initialize(AnalysisContext context)
+		{
+			context.RegisterCompilationStartAction(compilationStartAnalysisContext =>
+			{
+				var ifNotNullExtensionMethodType = compilationStartAnalysisContext.Compilation.GetTypeByMetadataName("Libronix.Utility.IfNotNull.IfNotNullExtensionMethod");
+				if (ifNotNullExtensionMethodType == null)
+					return;
+
+				var ifNotNullMethods = ifNotNullExtensionMethodType.GetMembers("IfNotNull");
+				if (ifNotNullMethods.Length == 0)
+					return;
+
+				compilationStartAnalysisContext.RegisterSyntaxNodeAction(c => AnalyzeSyntax(c, ifNotNullMethods), SyntaxKind.InvocationExpression);
+			});
+		}
+
+		public const string DiagnosticId = "FL0010";
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s_rule);
+
+		private static void AnalyzeSyntax(SyntaxNodeAnalysisContext context, ImmutableArray<ISymbol> ifNotNullMethods)
+		{
+			var syntax = (InvocationExpressionSyntax) context.Node;
+
+			var methodSymbol = context.SemanticModel.GetSymbolInfo(syntax.Expression).Symbol as IMethodSymbol;
+			if (methodSymbol == null ||
+				(methodSymbol.ReducedFrom == null && methodSymbol.ConstructedFrom == null) ||
+				!ifNotNullMethods.Any(x => x.Equals(methodSymbol.ReducedFrom) || (x.Equals(methodSymbol.ConstructedFrom))))
+				return;
+
+			context.ReportDiagnostic(Diagnostic.Create(s_rule, syntax.GetLocation()));
+		}
+
+		private static readonly DiagnosticDescriptor s_rule = new DiagnosticDescriptor(
+			id: DiagnosticId,
+			title: "IfNotNull deprecation",
+			messageFormat: "Prefer modern language features over IfNotNull usage.",
+			category: "Usage",
+			defaultSeverity: DiagnosticSeverity.Info,
+			isEnabledByDefault: true,
+			helpLinkUri: $"https://github.com/Faithlife/FaithlifeAnalyzers/wiki/{DiagnosticId}");
+	}
+}
---
+++ src/Faithlife.Analyzers/SyntaxUtility.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace Faithlife.Analyzers
+{
+	internal static class SyntaxUtility
+	{
+		/// <summary>
+		/// Generates an identifier name that has a high likelihood of not causing any naming conflicts.
+		/// </summary>
+		/// <param name="desiredName">The default name for the identifier.</param>
+		/// <param name="declarationLocation">A syntax location within the member in which the identifier will be declared.</param>
+		/// <param name="originalDeclaration">
+		/// An optional syntax node designating an existing variable declaration.
+		/// If specified, this node will be ignored when checking for uniqueness.
+		/// </param>
+		/// <returns>A <see cref="Microsoft.CodeAnalysis.SyntaxToken">SyntaxToken</see> for the new identifier.</returns>
+		/// <remarks>
+		/// Currently, this method takes a conservative approach to declarations within the member and pays no attention at all
+		/// to identifiers declared outside the scope of the member.
+		/// </remarks>
+		public static SyntaxToken GetHoistableIdentifier(string desiredName, SyntaxNode declarationLocation, SyntaxNode originalDeclaration = null)
+		{
+			if (desiredName is null)
+				throw new ArgumentNullException(nameof(desiredName));
+			if (declarationLocation is null)
+				throw new ArgumentNullException(nameof(declarationLocation));
+
+			var containingMember = declarationLocation.FirstAncestorOrSelf<MemberDeclarationSyntax>();
+			if (containingMember is null)
+				throw new InvalidOperationException("Cannot declare a variable at this scope.");
+
+			// Currently, this makes no attempt to be clever about scoping rules.
+			var unavailableNames = new HashSet<string>(
+				containingMember
+					.DescendantNodes()
+					.Select(node =>
+					{
+						if (node == originalDeclaration)
+							return default;
+						if (node is ParameterSyntax parameter)
+							return parameter.Identifier;
+						if (node is TypeParameterSyntax typeParameter)
+							return typeParameter.Identifier;
+						if (node is VariableDeclaratorSyntax variableDeclarator)
+							return variableDeclarator.Identifier;
+						if (node is SingleVariableDesignationSyntax singleVariableDesignation)
+							return singleVariableDesignation.Identifier;
+						if (node is FromClauseSyntax fromClause)
+							return fromClause.Identifier;
+						if (node is LetClauseSyntax letClause)
+							return letClause.Identifier;
+						if (node is QueryContinuationSyntax queryContinuation)
+							return queryContinuation.Identifier;
+
+						return default;
+					})
+					.Select(x => x.Text)
+					.Where(x => x?.StartsWith(desiredName, StringComparison.Ordinal) ?? false),
+				StringComparer.Ordinal);
+
+			// This will also make "value" unavailable within the get accessor.
+			if (containingMember is BasePropertyDeclarationSyntax)
+				unavailableNames.Add("value");
+
+			var suffix = 0;
+			var candidateName = desiredName;
+			while (unavailableNames.Contains(candidateName))
+				candidateName = desiredName + (++suffix).ToString(CultureInfo.InvariantCulture);
+
+			return Identifier(candidateName);
+		}
+	}
+}
