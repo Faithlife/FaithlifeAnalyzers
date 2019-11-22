beaa3f4097b4a2bcdddd9ccf945b481e61caf918
Bradley Grainger <bradley.grainger@logos.com>
2019-11-22 06:58:36 -0800

    Delete unused documentation template.

--- tools/Build/Build.cs
+++ tools/Build/Build.cs
@@ -9,12 +9,6 @@ internal static class Build
 			new DotNetBuildSettings
 			{
 				NuGetApiKey = Environment.GetEnvironmentVariable("NUGET_API_KEY"),
-				DocsSettings = new DotNetDocsSettings
-				{
-					GitLogin = new GitLoginInfo("faithlifebuildbot", Environment.GetEnvironmentVariable("BUILD_BOT_PASSWORD") ?? ""),
-					GitAuthor = new GitAuthorInfo("Faithlife Build Bot", "faithlifebuildbot@users.noreply.github.com"),
-					SourceCodeUrl = "https://github.com/Faithlife/RepoName/tree/master/src",
-				},
 				SourceLinkSettings = SourceLinkSettings.Default,
 			});
 	});
