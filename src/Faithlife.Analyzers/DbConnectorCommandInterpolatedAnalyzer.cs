4ee59aa55429450d909b6c849ea54e503a53e6ad
Ed Ball <ed.ball@faithlife.com>
2021-10-23 12:26:33 -0700

    Detect calling DbConnector.Command with an interpolated string.

---
+++ src/Faithlife.Analyzers/DbConnectorCommandInterpolatedAnalyzer.cs
@@ -0,0 +1,47 @@
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Faithlife.Analyzers
+{
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public sealed class DbConnectorCommandInterpolatedAnalyzer : DiagnosticAnalyzer
+	{
+		public override void Initialize(AnalysisContext context) =>
+			context.RegisterCompilationStartAction(compilationStartAnalysisContext =>
+			{
+				if (compilationStartAnalysisContext.Compilation.GetTypeByMetadataName("Faithlife.Data.DbConnector") is { } dbConnectorType)
+				{
+					compilationStartAnalysisContext.RegisterSyntaxNodeAction(syntaxNodeAnalysisContext =>
+					{
+						if (syntaxNodeAnalysisContext.Node is InvocationExpressionSyntax invocation &&
+							syntaxNodeAnalysisContext.SemanticModel.GetSymbolInfo(invocation.Expression).Symbol is IMethodSymbol method &&
+							method.ContainingType == dbConnectorType &&
+							method.Name == "Command" &&
+							method.Parameters.Length != 0 &&
+							method.Parameters[0].Type.SpecialType == SpecialType.System_String &&
+							invocation.ArgumentList.Arguments[0].Expression is InterpolatedStringExpressionSyntax)
+						{
+							syntaxNodeAnalysisContext.ReportDiagnostic(Diagnostic.Create(s_rule, invocation.GetLocation()));
+						}
+					}, SyntaxKind.InvocationExpression);
+				}
+			});
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s_rule);
+
+		public const string DiagnosticId = "FL0012";
+
+		private static readonly DiagnosticDescriptor s_rule = new(
+			id: DiagnosticId,
+			title: "Use DbConnector.CommandFormat",
+			messageFormat: "Command should not be used with an interpolated string; use CommandFormat instead.",
+			category: "Usage",
+			defaultSeverity: DiagnosticSeverity.Warning,
+			isEnabledByDefault: true,
+			helpLinkUri: $"https://github.com/Faithlife/FaithlifeAnalyzers/wiki/{DiagnosticId}"
+		);
+	}
+}
---
+++ tests/Faithlife.Analyzers.Tests/DbConnectorCommandInterpolatedTests.cs
@@ -0,0 +1,74 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using NUnit.Framework;
+
+namespace Faithlife.Analyzers.Tests
+{
+	[TestFixture]
+	public class DbConnectorCommandInterpolatedTests : CodeFixVerifier
+	{
+		[Test]
+		public void ValidUsage()
+		{
+			const string validProgram = c_preamble + @"
+namespace TestApplication
+{
+	internal static class TestClass
+	{
+		public static void UtilityMethod()
+		{
+			var connector = new DbConnector();
+			connector.Command(""valid"");
+		}
+	}
+}";
+
+			VerifyCSharpDiagnostic(validProgram);
+		}
+
+		[TestCase("connector.Command($\"invalid\");")]
+		[TestCase("connector.Command($\"invalid\", (\"value\", true));")]
+		public void InvalidUsage(string invalidCall)
+		{
+			var brokenProgram = c_preamble + @"
+namespace TestApplication
+{
+	internal static class TestClass
+	{
+		public static void UtilityMethod()
+		{
+			var connector = new DbConnector();
+			" + invalidCall + @"
+		}
+	}
+}";
+
+			var expected = new DiagnosticResult
+			{
+				Id = DbConnectorCommandInterpolatedAnalyzer.DiagnosticId,
+				Message = "Command should not be used with an interpolated string; use CommandFormat instead.",
+				Severity = DiagnosticSeverity.Warning,
+				Locations = new[] { new DiagnosticResultLocation("Test0.cs", s_preambleLength + 8, 4) },
+			};
+
+			VerifyCSharpDiagnostic(brokenProgram, expected);
+		}
+
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new DbConnectorCommandInterpolatedAnalyzer();
+
+		private const string c_preamble = @"using System;
+using Faithlife.Data;
+
+namespace Faithlife.Data
+{
+	public class DbConnector
+	{
+		public object Command(string text) => throw new NotImplementedException();
+		public object Command(string text, params (string Name, object Value)[] parameters) => throw new NotImplementedException();
+	}
+}
+";
+
+		private static readonly int s_preambleLength = c_preamble.Split('\n').Length;
+	}
+}
