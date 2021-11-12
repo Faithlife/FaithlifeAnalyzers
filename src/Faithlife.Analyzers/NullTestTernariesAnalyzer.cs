dc701c95cfe2b4480ed29e2abe1e416f36e938c2
Jeff Corcoran <jeff.corcoran@faithlife.com>
2021-11-12 08:26:13 -0500

    Initial checkin for Ternary null check analysis

---
+++ src/Faithlife.Analyzers/NullTestTernariesAnalyzer.cs
@@ -0,0 +1,53 @@
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Faithlife.Analyzers
+{
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public sealed class NullTestTernariesAnalyzer : DiagnosticAnalyzer
+	{
+		public override void Initialize(AnalysisContext context)
+		{
+			context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+			context.RegisterCompilationStartAction(compilationStartAnalysisContext =>
+			{
+				compilationStartAnalysisContext.RegisterSyntaxNodeAction(c => AnalyzeSyntax(c), SyntaxKind.ConditionalExpression);
+			});
+		}
+
+		private static void AnalyzeSyntax(SyntaxNodeAnalysisContext context)
+		{
+			var expression = (ConditionalExpressionSyntax) context.Node;
+			var falseValue = expression.WhenFalse;
+			var trueValue = expression.WhenTrue;
+
+			var eExpression = (BinaryExpressionSyntax) expression.Condition;
+
+			var lExpression = eExpression.Left;
+			var rExpression = eExpression.Right;
+
+			if ((falseValue.Kind() == SyntaxKind.NullLiteralExpression || trueValue.Kind() == SyntaxKind.NullLiteralExpression) && (lExpression.Kind() == SyntaxKind.NullLiteralExpression || rExpression.Kind() == SyntaxKind.NullLiteralExpression))
+			{
+				context.ReportDiagnostic(Diagnostic.Create(s_rule, expression.GetLocation()));
+			}
+		}
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s_rule);
+
+		public const string DiagnosticId = "FL0015";
+
+		private static readonly DiagnosticDescriptor s_rule = new(
+			id: DiagnosticId,
+			title: "Null Checking Ternaries Usage",
+			messageFormat: "Prefer null conditional operators over ternaries explicitly checking for null",
+			category: "Usage",
+			defaultSeverity: DiagnosticSeverity.Warning,
+			isEnabledByDefault: true,
+			helpLinkUri: $"https://github.com/Faithlife/FaithlifeAnalyzers/wiki/{DiagnosticId}"
+		);
+	}
+}
---
+++ tests/Faithlife.Analyzers.Tests/NullTestTernariesTests.cs
@@ -0,0 +1,75 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using NUnit.Framework;
+
+namespace Faithlife.Analyzers.Tests
+{
+	[TestFixture]
+	public class NullTestTernariesTests : CodeFixVerifier
+	{
+		[Test]
+		public void ValidUsage()
+		{
+			const string validProgram = c_preamble + @"
+namespace TestApplication
+{
+	public class Person
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+	}
+
+	internal static class TestClass
+	{
+		public static void UtilityMethod()
+		{
+			Person person = null;
+			var firstName = person?.FirstName;
+		}
+	}
+}";
+
+			VerifyCSharpDiagnostic(validProgram);
+		}
+
+		[TestCase("var firstName = person != null ? person.FirstName : null;")]
+		[TestCase("var firstName = person == null ? null : person.FirstName;")]
+		public void InvalidUsage(string badExample)
+		{
+			var brokenProgram = c_preamble + $@"
+namespace TestApplication
+{{
+	public class Person
+	{{
+		public string? FirstName {{ get; set; }}
+		public string? LastName {{ get; set; }}
+	}}
+
+	internal static class TestClass
+	{{
+		public static void UtilityMethod()
+		{{
+			Person person = null;
+			{badExample}
+		}}
+	}}
+}}";
+
+			var expected = new DiagnosticResult
+			{
+				Id = NullTestTernariesAnalyzer.DiagnosticId,
+				Message = "Prefer null conditional operators over ternaries explicitly checking for null",
+				Severity = DiagnosticSeverity.Warning,
+				Locations = new[] { new DiagnosticResultLocation("Test0.cs", s_preambleLength + 15, 20) },
+			};
+
+			VerifyCSharpDiagnostic(brokenProgram, expected);
+		}
+
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new NullTestTernariesAnalyzer();
+
+		private const string c_preamble = @"";
+
+		private static readonly int s_preambleLength = c_preamble.Split('\n').Length;
+	}
+}
