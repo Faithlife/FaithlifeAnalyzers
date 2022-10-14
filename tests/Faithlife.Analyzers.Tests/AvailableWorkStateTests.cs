Faithlife/FaithlifeAnalyzers

commit fcb6c2e6f6e81293749f72629668ba987d4637d3

Defer calling SemanticModel.GetSymbolInfo.
This method call can be extremely expensive; invoking it too frequently can slow down builds dramatically. Instead, try to find an early out before getting the symbol info.

src/Faithlife.Analyzers/AvailableWorkStateAnalyzer.cs
@@ -44,14 +44,6 @@ namespace Faithlife.Analyzers
 		{
 			var syntax = (MemberAccessExpressionSyntax)context.Node;
 
-			var symbolInfo = context.SemanticModel.GetSymbolInfo(syntax.Expression);
-			if (symbolInfo.Symbol == null || !symbolInfo.Symbol.Equals(workStateClass))
-				return;
-
-			var memberSymbolInfo = context.SemanticModel.GetSymbolInfo(syntax.Name);
-			if (memberSymbolInfo.Symbol == null || (!memberSymbolInfo.Symbol.Equals(workStateNone) && !memberSymbolInfo.Symbol.Equals(workStateToDo)))
-				return;
-
 			var containingMethod = syntax.Ancestors().OfType<MethodDeclarationSyntax>().FirstOrDefault();
 			if (containingMethod == null)
 				return;
@@ -68,6 +60,14 @@ namespace Faithlife.Analyzers
 					return;
 				}
 			}
+
+			var symbolInfo = context.SemanticModel.GetSymbolInfo(syntax.Expression);
+			if (symbolInfo.Symbol == null || !symbolInfo.Symbol.Equals(workStateClass))
+				return;
+
+			var memberSymbolInfo = context.SemanticModel.GetSymbolInfo(syntax.Name);
+			if (memberSymbolInfo.Symbol == null || (!memberSymbolInfo.Symbol.Equals(workStateNone) && !memberSymbolInfo.Symbol.Equals(workStateToDo)))
+				return;
 
 			var cancellationToken = context.SemanticModel.Compilation.GetTypeByMetadataName("System.Threading.CancellationToken");
 			var asyncMethodContext = context.SemanticModel.Compilation.GetTypeByMetadataName("Libronix.Utility.Threading.AsyncMethodContext");

src/Faithlife.Analyzers/CurrentAsyncWorkItemAnalyzer.cs
@@ -40,14 +40,6 @@ namespace Faithlife.Analyzers
 		{
 			var syntax = (MemberAccessExpressionSyntax) context.Node;
 
-			var symbolInfo = context.SemanticModel.GetSymbolInfo(syntax.Expression);
-			if (symbolInfo.Symbol == null || !symbolInfo.Symbol.Equals(asyncWorkItem))
-				return;
-
-			var memberSymbolInfo = context.SemanticModel.GetSymbolInfo(syntax.Name);
-			if (memberSymbolInfo.Symbol == null || !memberSymbolInfo.Symbol.Equals(asyncWorkItemCurrent))
-				return;
-
 			var containingMethod = syntax.Ancestors().OfType<MethodDeclarationSyntax>().FirstOrDefault();
 			if (containingMethod == null)
 				return;
@@ -59,6 +51,14 @@ namespace Faithlife.Analyzers
 			{
 				return;
 			}
+
+			var symbolInfo = context.SemanticModel.GetSymbolInfo(syntax.Expression);
+			if (symbolInfo.Symbol == null || !symbolInfo.Symbol.Equals(asyncWorkItem))
+				return;
+
+			var memberSymbolInfo = context.SemanticModel.GetSymbolInfo(syntax.Name);
+			if (memberSymbolInfo.Symbol == null || !memberSymbolInfo.Symbol.Equals(asyncWorkItemCurrent))
+				return;
 
 			// check for AsyncWorkItem.Current being used in a lambda passed as an argument to AsyncWorkItem.Start
 			var invocation = syntax.FirstAncestorOrSelf<LambdaExpressionSyntax>()?.FirstAncestorOrSelf<ArgumentSyntax>()?.FirstAncestorOrSelf<InvocationExpressionSyntax>();

src/Faithlife.Analyzers/EmptyStringAnalyzer.cs
@@ -38,6 +38,9 @@ namespace Faithlife.Analyzers
 		{
 			var syntax = (MemberAccessExpressionSyntax)context.Node;
 
+			if (syntax.Name.Identifier.Text != "Empty")
+				return;
+
 			var symbolInfo = context.SemanticModel.GetSymbolInfo(syntax.Name);
 			if (symbolInfo.Symbol == null || !symbolInfo.Symbol.Equals(emptyString))
 				return;

src/Faithlife.Analyzers/UntilCanceledAnalyzer.cs
@@ -39,8 +39,18 @@ namespace Faithlife.Analyzers
 			{
 				var invocation = (InvocationExpressionSyntax) context.Node;
 
+				var name = invocation.Expression switch
+				{
+					MemberAccessExpressionSyntax memberAccess => memberAccess.Name,
+					MemberBindingExpressionSyntax memberBinding => memberBinding.Name,
+					_ => null,
+				};
+
+				if (name?.Identifier.Text != "UntilCanceled")
+					return;
+
 				var method = context.SemanticModel.GetSymbolInfo(invocation.Expression).Symbol as IMethodSymbol;
-				if (method?.Name != "UntilCanceled" || method.ContainingType != asyncEnumerableUtility)
+				if (method?.ContainingType != asyncEnumerableUtility)
 					return;
 
 				if (invocation.ArgumentList.Arguments.Count != 0)

commit ae8a072f44705fadca46fb4579ea52280d8488be

Fix false positive for FL0008.
This diagnostic was incorrectly reported on any property access within an IEnumerable<AsyncAction> method. The bug was introduced in fcb6c2e6f6e81293749f72629668ba987d4637d3.

Directory.Build.props
@@ -1,7 +1,7 @@
 <Project>
 
   <PropertyGroup>
-    <VersionPrefix>1.3.0</VersionPrefix>
+    <VersionPrefix>1.3.1</VersionPrefix>
     <LangVersion>10.0</LangVersion>
     <Nullable>enable</Nullable>
     <ImplicitUsings>enable</ImplicitUsings>

src/Faithlife.Analyzers/AvailableWorkStateAnalyzer.cs
@@ -1,9 +1,9 @@
 using System.Collections.Immutable;
-using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
 
 namespace Faithlife.Analyzers;
 
@@ -32,7 +32,7 @@ public sealed class AvailableWorkStateAnalyzer : DiagnosticAnalyzer
 			if (workStateToDo.Length != 1)
 				return;
 
-			compilationStartAnalysisContext.RegisterSyntaxNodeAction(c => AnalyzeSyntax(c, iworkState, workStateClass, workStateNone[0], workStateToDo[0]), SyntaxKind.SimpleMemberAccessExpression);
+			compilationStartAnalysisContext.RegisterOperationAction(x => AnalyzeOperation(x, iworkState, workStateNone[0], workStateToDo[0]), OperationKind.PropertyReference);
 		});
 	}
 
@@ -40,44 +40,39 @@ public sealed class AvailableWorkStateAnalyzer : DiagnosticAnalyzer
 
 	public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s_rule);
 
-	private static void AnalyzeSyntax(SyntaxNodeAnalysisContext context, INamedTypeSymbol iworkState, INamedTypeSymbol workStateClass, ISymbol workStateNone, ISymbol workStateToDo)
+	private static void AnalyzeOperation(OperationAnalysisContext context, INamedTypeSymbol iworkState, ISymbol workStateNone, ISymbol workStateToDo)
 	{
-		var syntax = (MemberAccessExpressionSyntax)context.Node;
+		var propertyReferenceOperation = (IPropertyReferenceOperation) context.Operation;
+		if (!propertyReferenceOperation.Property.Equals(workStateNone) && !propertyReferenceOperation.Property.Equals(workStateToDo))
+			return;
+
+		var syntax = (MemberAccessExpressionSyntax) propertyReferenceOperation.Syntax;
 
 		var containingMethod = syntax.Ancestors().OfType<MethodDeclarationSyntax>().FirstOrDefault();
 		if (containingMethod == null)
 			return;
 
-		var asyncAction = context.SemanticModel.Compilation.GetTypeByMetadataName("Libronix.Utility.Threading.AsyncAction");
-		if (asyncAction != null)
+		var semanticModel = context.Compilation.GetSemanticModel(context.Operation.Syntax.SyntaxTree);
+
+		var asyncAction = context.Compilation.GetTypeByMetadataName("Libronix.Utility.Threading.AsyncAction");
+		var ienumerable = context.Compilation.GetTypeByMetadataName("System.Collections.Generic.IEnumerable`1");
+		var returnTypeSymbol = semanticModel.GetSymbolInfo(containingMethod.ReturnType).Symbol as INamedTypeSymbol;
+		var returnsIEnumerableAsyncAction = ienumerable?.Equals(returnTypeSymbol?.ConstructedFrom) is true && asyncAction?.Equals(returnTypeSymbol.TypeArguments[0]) is true;
+		if (returnsIEnumerableAsyncAction)
 		{
-			var ienumerable = context.SemanticModel.Compilation.GetTypeByMetadataName("System.Collections.Generic.IEnumerable`1");
-			var returnTypeSymbol = context.SemanticModel.GetSymbolInfo(containingMethod.ReturnType).Symbol as INamedTypeSymbol;
-			if (returnTypeSymbol != null && returnTypeSymbol.ConstructedFrom != null && returnTypeSymbol.ConstructedFrom.Equals(ienumerable) &&
-				returnTypeSymbol.TypeArguments[0].Equals(asyncAction))
-			{
-				context.ReportDiagnostic(Diagnostic.Create(s_rule, syntax.GetLocation()));
-				return;
-			}
+			context.ReportDiagnostic(Diagnostic.Create(s_rule, syntax.GetLocation()));
+			return;
 		}
 
-		var symbolInfo = context.SemanticModel.GetSymbolInfo(syntax.Expression);
-		if (symbolInfo.Symbol == null || !symbolInfo.Symbol.Equals(workStateClass))
-			return;
-
-		var memberSymbolInfo = context.SemanticModel.GetSymbolInfo(syntax.Name);
-		if (memberSymbolInfo.Symbol == null || (!memberSymbolInfo.Symbol.Equals(workStateNone) && !memberSymbolInfo.Symbol.Equals(workStateToDo)))
-			return;
-
-		var cancellationToken = context.SemanticModel.Compilation.GetTypeByMetadataName("System.Threading.CancellationToken");
-		var asyncMethodContext = context.SemanticModel.Compilation.GetTypeByMetadataName("Libronix.Utility.Threading.AsyncMethodContext");
+		var cancellationToken = context.Compilation.GetTypeByMetadataName("System.Threading.CancellationToken");
+		var asyncMethodContext = context.Compilation.GetTypeByMetadataName("Libronix.Utility.Threading.AsyncMethodContext");
 		var hasWorkStateParameters = containingMethod.ParameterList.Parameters.Any(parameter =>
 		{
-			var parameterSymbolInfo = context.SemanticModel.GetSymbolInfo(parameter.Type);
+			var parameterSymbolInfo = semanticModel.GetSymbolInfo(parameter.Type);
 			if (parameterSymbolInfo.Symbol is null)
 				return false;
 
-			if (symbolInfo.Symbol.Equals(iworkState))
+			if (parameterSymbolInfo.Symbol.Equals(iworkState))
 				return true;
 
 			var namedTypeSymbol = parameterSymbolInfo.Symbol as INamedTypeSymbol;
@@ -93,7 +88,7 @@ public sealed class AvailableWorkStateAnalyzer : DiagnosticAnalyzer
 		if (!hasWorkStateParameters)
 			return;
 
-		context.ReportDiagnostic(Diagnostic.Create(s_rule, syntax.GetLocation()));
+		context.ReportDiagnostic(Diagnostic.Create(s_rule, propertyReferenceOperation.Syntax.GetLocation()));
 	}
 
 	private static readonly DiagnosticDescriptor s_rule = new DiagnosticDescriptor(

tests/Faithlife.Analyzers.Tests/AvailableWorkStateTests.cs
@@ -8,6 +8,26 @@ namespace Faithlife.Analyzers.Tests;
 [TestFixture]
 public class AvailableWorkStateTests : CodeFixVerifier
 {
+	[Test]
+	public void NoDiagnosticWhenNotUsed()
+	{
+		string validProgram = preamble + @"
+namespace TestApplication
+{
+	internal class TestClass
+	{
+		public static IEnumerable<AsyncAction> Method(TestClass testClass)
+		{
+			testClass.Property = 1;
+			yield break;
+		}
+
+		public int Property { get; set; }
+	}
+}";
+		VerifyCSharpDiagnostic(validProgram);
+	}
+
 	[TestCase("None")]
 	[TestCase("ToDo")]
 	public void ValidUsage(string property)