bd8a549b8133e2d9df3c0c96126ead5fe4d4f3dd
Dave Dunkin <dave.dunkin@logos.com>
2022-10-31 11:53:40 -0700

    Add FL0017 ConstantSwitchAnalyzer. Closes #77.

---
+++ src/Faithlife.Analyzers/ConstantSwitchAnalyzer.cs
@@ -0,0 +1,41 @@
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Faithlife.Analyzers;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class ConstantSwitchAnalyzer : DiagnosticAnalyzer
+{
+	public override void Initialize(AnalysisContext context)
+	{
+		context.EnableConcurrentExecution();
+		context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+		context.RegisterCompilationStartAction(compilationStartAnalysisContext =>
+		{
+			compilationStartAnalysisContext.RegisterOperationAction(AnalyzeOperation, OperationKind.SwitchExpression);
+		});
+	}
+
+	public const string DiagnosticId = "FL0017";
+
+	public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s_rule);
+
+	private static void AnalyzeOperation(OperationAnalysisContext context)
+	{
+		var operation = (ISwitchExpressionOperation) context.Operation;
+		if (operation.Value.ConstantValue.HasValue)
+			context.ReportDiagnostic(Diagnostic.Create(s_rule, context.Operation.Syntax.GetLocation()));
+	}
+
+	private static readonly DiagnosticDescriptor s_rule = new DiagnosticDescriptor(
+		id: DiagnosticId,
+		title: "Do not switch on a constant value",
+		messageFormat: "Do not use a constant value as the target of a switch expression",
+		category: "Usage",
+		defaultSeverity: DiagnosticSeverity.Warning,
+		isEnabledByDefault: true,
+		helpLinkUri: $"https://github.com/Faithlife/FaithlifeAnalyzers/wiki/{DiagnosticId}");
+}
---
+++ tests/Faithlife.Analyzers.Tests/ConstantSwitchTests.cs
@@ -0,0 +1,67 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using NUnit.Framework;
+
+namespace Faithlife.Analyzers.Tests;
+
+[TestFixture]
+public sealed class ConstantSwitchTests : CodeFixVerifier
+{
+	[Test]
+	public void ValidSwitchExpression()
+	{
+		const string validProgram = @"using System;
+
+namespace TestApplication
+{
+	public class TestClass
+	{
+		public TestClass()
+		{
+			int? i = 0;
+			var zero = i switch
+			{
+				0 => true,
+				1 => false,
+				_ => throw new InvalidOperationException(),
+			};
+		}
+	}
+}";
+		VerifyCSharpDiagnostic(validProgram);
+	}
+
+	[Test]
+	public void InvalidSwitchExpression()
+	{
+		const string invalidProgram = @"using System;
+
+namespace TestApplication
+{
+	public class TestClass
+	{
+		public TestClass()
+		{
+			int? i = 0;
+			var zero = 0 switch
+			{
+				_ when i is 0 => true,
+				_ when i is 1 => false,
+				_ => throw new InvalidOperationException(),
+			};
+		}
+	}
+}";
+		var expected = new DiagnosticResult
+		{
+			Id = ConstantSwitchAnalyzer.DiagnosticId,
+			Message = "Do not use a constant value as the target of a switch expression",
+			Severity = DiagnosticSeverity.Warning,
+			Locations = new[] { new DiagnosticResultLocation("Test0.cs", 10, 15) },
+		};
+
+		VerifyCSharpDiagnostic(invalidProgram, expected);
+	}
+
+	protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new ConstantSwitchAnalyzer();
+}
