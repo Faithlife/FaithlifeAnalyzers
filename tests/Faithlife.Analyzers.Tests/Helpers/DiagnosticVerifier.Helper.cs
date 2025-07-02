fe0a4642de1139aa2c6ff97c50cdd2c996e4e206
Bradley Grainger <bgrainger@gmail.com>
2025-07-02 13:42:30 -0700

    Delete unwanted added line.

--- tests/Faithlife.Analyzers.Tests/Helpers/DiagnosticVerifier.Helper.cs
+++ tests/Faithlife.Analyzers.Tests/Helpers/DiagnosticVerifier.Helper.cs
@@ -161,7 +161,6 @@ public abstract partial class DiagnosticVerifier
 			.CurrentSolution
 			.AddProject(projectId, TestProjectName, TestProjectName, language)
 			.WithProjectCompilationOptions(projectId, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))
-			.WithProjectParseOptions(projectId, new CSharpParseOptions(LanguageVersion.Latest))
 			.AddMetadataReferences(projectId, s_metadataReferences);
 
 		int count = 0;
